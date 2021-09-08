f881aa7e67a9b1859f4ddf8ca902a777547adad4
= <=>
2021-09-08 10:33:02 -0500

    Se agrego el sembrador de Usuarios

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create(
+            [
+                'name' => 'Edier Hernández',
+                'email' => 'edierhernandezmo@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
+            ]
+        );
+    }
+}
